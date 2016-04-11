f5b31574671f41c314cd8e989f6020c912d467f4
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-11 19:45:55 +0300

    Main Activity

---
+++ R.can.O.E.D/app/src/main/java/github/johnnysc/rcanoed/MainActivity.java
@@ -0,0 +1,88 @@
+package github.johnnysc.rcanoed;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private static final int MENU_RESUME = 1;
+    private static final int MENU_START = 2;
+    private static final int MENU_STOP = 3;
+
+    private GameThread mGameThread;
+    private GameView mGameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+
+        mGameView = (GameView)findViewById(R.id.gamearea);
+        mGameView.setStatusView((TextView)findViewById(R.id.text));
+        mGameView.setScoreView((TextView)findViewById(R.id.score));
+
+        this.startGame(mGameView, null, savedInstanceState);
+    }
+
+    private void startGame(GameView gView, GameThread gThread, Bundle savedInstanceState) {
+        mGameThread = new rcanoedGame(mGameView);
+        mGameView.setThread(mGameThread);
+        mGameThread.setState(GameThread.STATE_READY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(mGameThread.getMode() == GameThread.STATE_RUNNING) {
+            mGameThread.setState(GameThread.STATE_PAUSE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mGameView.cleanup();
+        mGameThread = null;
+        mGameView = null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, MENU_START, 0, R.string.menu_start);
+        menu.add(0, MENU_STOP, 0, R.string.menu_stop);
+        menu.add(0, MENU_RESUME, 0, R.string.menu_resume);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_START:
+                mGameThread.doStart();
+                return true;
+            case MENU_STOP:
+                mGameThread.setState(GameThread.STATE_LOSE,  getText(R.string.message_stopped));
+                return true;
+            case MENU_RESUME:
+                mGameThread.unpause();
+                return true;
+        }
+
+        return false;
+    }
+}

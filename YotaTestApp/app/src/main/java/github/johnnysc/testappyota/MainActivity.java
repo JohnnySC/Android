5a820512c495d73fc2a22a0cfe0808affdf1e5f2
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-06-21 16:15:58 +0300

    Test App Yota

---
+++ YotaTestApp/app/src/main/java/github/johnnysc/testappyota/MainActivity.java
@@ -0,0 +1,61 @@
+package github.johnnysc.testappyota;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/* Test App for Yota
+* App gets url address and shows the html code of it.
+* Made by Hovhannes Asatryan on 21.06.16
+*/
+public class MainActivity extends Activity{
+    public static EditText editText;
+    public static Button button;
+    public static TextView textView;
+    public static StringBuilder sb = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                (new Thread(new HtmlRunnable())).start();
+                textView.setText(sb.toString());
+            }
+        });
+    }
+
+public class HtmlRunnable implements Runnable {
+    public void run(){
+        try {
+            URL pageURL = new URL(editText.getText().toString());
+            URLConnection  uc = pageURL.openConnection();
+            BufferedReader   br = new BufferedReader(
+                                  new InputStreamReader(
+                                       uc.getInputStream(), "UTF-8"));
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                sb.append(inputLine);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+}

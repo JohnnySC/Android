8cb08a388dfe0f80f406ec44e9c1658c67e3b129
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-06-25 17:14:35 +0300

    Fixed the bugs in case if no data obtained.

--- TestAppINTECH/app/src/main/java/github/johnnysc/testappintech/PlayerActivity.java
+++ TestAppINTECH/app/src/main/java/github/johnnysc/testappintech/PlayerActivity.java
@@ -5,37 +5,38 @@ import android.graphics.Bitmap;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import java.io.IOException;
 
 /**
  * Created by Hovhannes Asatryan on 23.06.16.
  */
 public class PlayerActivity extends Activity {
-    public static ImageView albumCoverView;
-    public static TextView songView;
-    public static TextView artistNameView;
-    public static Bitmap bitmap=null;
-    private Song song = null;
-    private Button playButton;
-    private Button pauseButton;
+    ImageView albumCoverView;
+    TextView songView;
+    TextView artistNameView;
+    Bitmap bitmap=null;
+    Song song = null;
+    Button playButton;
+    Button pauseButton;
     boolean isPlaying = false;
-    private  MediaPlayer mp=null;
-    private SeekBar seekBar;
-    private Handler myHandler = new Handler();
-    private double startTime = 0;
-    private double finalTime = 0;
-    public static int oneTimeOnly;
+    MediaPlayer mp=null;
+    SeekBar seekBar;
+    Handler myHandler = new Handler();
+    double startTime = 0;
+    double finalTime = 0;
+    int oneTimeOnly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_activity);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         albumCoverView = (ImageView)findViewById(R.id.playerAlbumCoverView);
         songView = (TextView)findViewById(R.id.playerSongTextView);
@@ -46,61 +47,50 @@ public class PlayerActivity extends Activity {
         song = SongAdapter.allSongs.get(MainActivity.positionOfItem);
 
         Thread thread = new Thread(new bitmapRunnable());
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        albumCoverView.setImageBitmap(bitmap);
+        MainActivity.runTheThread(thread);
 
+        albumCoverView.setImageBitmap(bitmap);
         songView.setText(song.getTrackName());
         artistNameView.setText(song.getArtistName());
-
-
 
         seekBar = (SeekBar)findViewById(R.id.seekBar);
         seekBar.setClickable(false);
         pauseButton.setEnabled(false);
-
         mp = new MediaPlayer();
-
         oneTimeOnly =0;
+
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (!isPlaying) {
-                    isPlaying = true;
-
-                    try {
-                        if(oneTimeOnly==0) {
+                try {
+                    if (!isPlaying) {
+                        isPlaying = true;
+                        if (oneTimeOnly == 0) {
                             mp.setDataSource(song.getSongURL());
                             mp.prepare();
                         }
                         mp.start();
                         finalTime = mp.getDuration();
-
-                        if(startTime==finalTime)
-                            startTime=0;
+                        if (startTime == finalTime)
+                            startTime = 0;
                         else
-                        startTime = mp.getCurrentPosition();
-
+                            startTime = mp.getCurrentPosition();
                         if (oneTimeOnly == 0) {
                             seekBar.setMax((int) finalTime);
                             oneTimeOnly = 1;
                         }
-                        seekBar.setProgress((int)startTime);
-                        myHandler.postDelayed(UpdateSongTime,100);
+                        seekBar.setProgress((int) startTime);
+                        myHandler.postDelayed(UpdateSongTime, 100);
                         pauseButton.setEnabled(true);
                         playButton.setEnabled(false);
-                    } catch (IOException e) {
-                        Log.e("Log_Tag","prepare() failed");
+
+                    } else {
+                        isPlaying = false;
+                        mp.release();
+                        mp = null;
                     }
-                } else {
-                    isPlaying = false;
-                    mp.release();
-                    mp = null;
+                }catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -114,7 +104,6 @@ public class PlayerActivity extends Activity {
                 pauseButton.setEnabled(false);
             }
         });
-
     }
 
     private Runnable UpdateSongTime = new Runnable(){
@@ -135,6 +124,7 @@ public class PlayerActivity extends Activity {
     public void onBackPressed() {
         super.onBackPressed();
         myHandler.removeCallbacks(UpdateSongTime);
+        if(mp!=null)
         mp.release();
     }
 }

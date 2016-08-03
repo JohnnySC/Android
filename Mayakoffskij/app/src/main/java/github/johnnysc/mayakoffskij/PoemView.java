6c34f49db91f99e1380f1afc932ac46493c93595
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-08-03 19:58:47 +0300

    Refactoring

--- Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/PoemView.java
+++ Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/PoemView.java
@@ -13,14 +13,19 @@ import java.io.FileNotFoundException;
 /*
  * Created by Hovhannes Asatryan (https://github.com/JohnnySC) on 05.05.16.
  */
-public class PoemView extends Activity {
+public class PoemView extends Activity implements View.OnClickListener {
+    TextView aPoemView;
+    Button nextButton;
+    Button previousButton;
+    Button addToFavorites;
+    Button removeFromFavorites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poem_view);
 
-        TextView aPoemView = (TextView)findViewById(R.id.aPoemTextView);
+        initUI();
         if(FavoritePoems.favCode==1)
             aPoemView.setText(Poems.getPoem(FavoritePoems.favPosition));
         else
@@ -28,70 +33,50 @@ public class PoemView extends Activity {
 
         aPoemView.setTextSize(MainActivity.fontSize);
 
-        final Button nextButton = (Button)findViewById(R.id.nextButton);
-        final Button previousButton = (Button)findViewById(R.id.previousButton);
-
-        final Button addToFavorites = (Button)findViewById(R.id.add_to_favorites);
-        final Button removeFromFavorites = (Button)findViewById(R.id.remove_from_favorites);
-
         if(FavoritePoems.favCode==0) {
             removeFromFavorites.setVisibility(View.GONE);
-            if(MainActivity.positionOfItem < MainActivity.allPoems.getSize() - 1){
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.positionOfItem++;
-                Intent intent = new Intent(PoemView.this, PoemView.class);
-                startActivityForResult(intent, 0);
-                finish();
-            }
-        });
-            }else
+            if(MainActivity.positionOfItem < MainActivity.allPoems.getSize() - 1)
+                nextButton.setOnClickListener(this);
+            else
                 nextButton.setVisibility(View.GONE);
         }else {
             removeFromFavorites.setVisibility(View.VISIBLE);
-            removeFromFavorites.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FavoritePoemsAdapter.favoritePoems.remove(PoemAdapter.poems.get(FavoritePoems.favPosition));
-                    for(int i=0;i<FavoritePoems.favIndexes.size();i++) {
-                        if (FavoritePoems.favPosition == FavoritePoems.favIndexes.get(i)){
-                            FavoritePoems.favIndexes.remove(i);
-                        Toast.makeText(getApplicationContext(), "Удалено из избранных", Toast.LENGTH_SHORT).show();
-                            try {
-                                FileIO.writeData(getApplicationContext(),openFileOutput(FileIO.file_name,MODE_PRIVATE));
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    FavoritePoems.favoritePoemsActivity.finish();
-                    Intent intent = new Intent(PoemView.this,FavoritePoems.class);
-                    startActivityForResult(intent,0);
-                    finish();
-                }
-            });
+            removeFromFavorites.setOnClickListener(this);
             addToFavorites.setVisibility(View.GONE);
             nextButton.setVisibility(View.GONE);
             previousButton.setVisibility(View.GONE);
         }
 
-        if(MainActivity.positionOfItem>0) {
-            previousButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    MainActivity.positionOfItem--;
-                    Intent intent = new Intent(PoemView.this, PoemView.class);
-                    startActivityForResult(intent, 0);
-                    finish();
-                }
-            });
-        }else
+        if(MainActivity.positionOfItem>0)
+            previousButton.setOnClickListener(this);
+        else
         previousButton.setVisibility(View.GONE);
 
-        addToFavorites.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        addToFavorites.setOnClickListener(this);
+    }
+
+    private void initUI() {
+        aPoemView = (TextView)findViewById(R.id.aPoemTextView);
+        nextButton = (Button)findViewById(R.id.nextButton);
+        previousButton = (Button)findViewById(R.id.previousButton);
+        addToFavorites = (Button)findViewById(R.id.add_to_favorites);
+        removeFromFavorites = (Button)findViewById(R.id.remove_from_favorites);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.nextButton:
+                MainActivity.positionOfItem++;
+                startNewActivity(PoemView.class);
+                break;
+
+            case R.id.previousButton:
+                MainActivity.positionOfItem--;
+                startNewActivity(PoemView.class);
+                break;
+
+            case R.id.add_to_favorites:
                 if(!FavoritePoems.favIndexes.contains(MainActivity.positionOfItem)) {
                     FavoritePoems.favIndexes.add(MainActivity.positionOfItem);
                     FavoritePoemsAdapter.favoritePoems.add(PoemAdapter.poems.get(MainActivity.positionOfItem));
@@ -103,7 +88,30 @@ public class PoemView extends Activity {
                     }
                 }else
                     Toast.makeText(getApplicationContext(),"Уже содержится в избранных",Toast.LENGTH_LONG).show();
-            }
-        });
+                break;
+
+            case R.id.remove_from_favorites:
+                FavoritePoemsAdapter.favoritePoems.remove(PoemAdapter.poems.get(FavoritePoems.favPosition));
+                for(int i=0;i<FavoritePoems.favIndexes.size();i++) {
+                    if (FavoritePoems.favPosition == FavoritePoems.favIndexes.get(i)){
+                        FavoritePoems.favIndexes.remove(i);
+                        Toast.makeText(getApplicationContext(), "Удалено из избранных", Toast.LENGTH_SHORT).show();
+                        try {
+                            FileIO.writeData(getApplicationContext(),openFileOutput(FileIO.file_name,MODE_PRIVATE));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                FavoritePoems.favoritePoemsActivity.finish();
+                startNewActivity(FavoritePoems.class);
+                break;
+        }
+    }
+
+    private void startNewActivity(Class another){
+        Intent intent = new Intent(PoemView.this, another);
+        startActivity(intent);
+        finish();
     }
 }

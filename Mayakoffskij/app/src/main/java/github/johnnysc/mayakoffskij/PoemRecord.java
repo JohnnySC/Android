615250e224e1423bf845c2d1f4c61d5c508169b9
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-08-03 19:59:16 +0300

    removed setters

--- Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/PoemRecord.java
+++ Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/PoemRecord.java
@@ -13,7 +13,5 @@ public class PoemRecord {
     }
 
     public String getName(){ return name;}
-    public void setName(String name){this.name = name;}
     public String getPoem(){ return poem;}
-    public void setPoem(String poem){ this.poem = poem;}
 }

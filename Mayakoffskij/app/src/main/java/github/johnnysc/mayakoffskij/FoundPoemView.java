36745076c72c6c08b3f48031b2e94513ae0ce924
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-08-09 15:16:44 +0300

    ButterKnife refactoring

--- Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/FoundPoemView.java
+++ Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/FoundPoemView.java
@@ -7,40 +7,45 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /*
  * Created by Hovhannes Asatryan (https://github.com/JohnnySC) on 05.05.16.
  */
 public class FoundPoemView extends Activity {
+    @BindView(R.id.aPoemTextView) TextView aPoemView;
+    @BindView(R.id.nextButton) Button nextButton;
+    @BindView(R.id.previousButton) Button previousButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poem_view);
 
-        TextView aPoemView = (TextView)findViewById(R.id.aPoemTextView);
+        ButterKnife.bind(this);
         aPoemView.setText(Poems.getPoem(SearchResultsList.foundPosition));
         aPoemView.setTextSize(MainActivity.fontSize);
 
-        final Button nextButton = (Button)findViewById(R.id.nextButton);
         nextButton.setVisibility(View.GONE);
-
-        final Button previousButton = (Button)findViewById(R.id.previousButton);
         previousButton.setVisibility(View.GONE);
+    }
 
-        final Button addToFavorites = (Button)findViewById(R.id.add_to_favorites);
-
-        addToFavorites.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    if(!FavoritePoems.favIndexes.contains(SearchResultsList.foundPosition)) {
-                        FavoritePoems.favIndexes.add(SearchResultsList.foundPosition);
-                        FavoritePoemsAdapter.favoritePoems.add(PoemAdapter.poems.get(SearchResultsList.foundPosition));
-                        Toast.makeText(getApplicationContext(),"Добавлено в избранное",Toast.LENGTH_SHORT).show();
-                    }else
-                        Toast.makeText(getApplicationContext(),"Уже содержится в избранных",Toast.LENGTH_LONG).show();
-
-
+    @OnClick(R.id.add_to_favorites)
+    void addToFavorites(){
+        if(!FavoritePoems.favIndexes.contains(SearchResultsList.foundPosition)) {
+            FavoritePoems.favIndexes.add(SearchResultsList.foundPosition);
+            FavoritePoemsAdapter.favoritePoems.add(PoemAdapter.poems.get(SearchResultsList.foundPosition));
+            Toast.makeText(getApplicationContext(),"Добавлено в избранное",Toast.LENGTH_SHORT).show();
+            try {
+                FileIO.writeData(getApplicationContext(),openFileOutput(FileIO.file_name,MODE_PRIVATE));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-        });
+        }else
+            Toast.makeText(getApplicationContext(),"Уже содержится в избранных",Toast.LENGTH_LONG).show();
     }
 }

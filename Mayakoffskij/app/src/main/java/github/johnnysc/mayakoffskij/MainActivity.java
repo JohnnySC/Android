99054d2199e1b4a7ccf8163f1975412fea3fa91e
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-08-09 15:16:59 +0300

    ButterKnife refactoring

--- Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/MainActivity.java
+++ Mayakoffskij/app/src/main/java/github/johnnysc/mayakoffskij/MainActivity.java
@@ -13,34 +13,34 @@ import android.widget.TextView;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /*
  * Created by Hovhannes Asatryan (https://github.com/JohnnySC) on 05.05.16.
  */
 
-public class MainActivity extends Activity implements View.OnClickListener{
-    public static PoemAdapter poemAdapter;
+public class MainActivity extends Activity{
+    PoemAdapter poemAdapter;
     public static int positionOfItem;
-    Button searchButton;
-    public static EditText searchText;
-    ListView listView;
     public static Poems allPoems;
-    Button goToFavorites;
-    TextView allPoemsCount;
-    Button fontSizeMinus;
-    Button fontSizePlus;
     public static int fontSize;
-    TextView fontExample;
+    @BindView(R.id.searchText) EditText searchText;
+    @BindView(R.id.poems_list) ListView listView;
+    @BindView(R.id.allPoemsCount) TextView allPoemsCount;
+    @BindView(R.id.fontexample) TextView fontExample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
         FavoritePoems.favIndexes = new ArrayList<>();
         positionOfItem = 0;
         fontSize = 20;
 
-        initUI();
         poemAdapter = new PoemAdapter();
         listView.setAdapter(poemAdapter);
         allPoems = new Poems();
@@ -49,13 +49,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         fontExample.setTextSize(fontSize);
 
-        setButtonsListener();
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             public void onItemClick(AdapterView<?> parent,View view,int position,long id){
                 positionOfItem = position;
                 FavoritePoems.favCode = 0;
-                startNewActivity(PoemView.class);
+                startNewActivity(PoemView.class,false);
             }
         }
         );
@@ -68,40 +66,21 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     }
 
-    private void initUI() {
-        listView = (ListView)findViewById(R.id.poems_list);
-        allPoemsCount = (TextView)findViewById(R.id.allPoemsCount);
-        searchButton = (Button)findViewById(R.id.searchButton);
-        searchText = (EditText)findViewById(R.id.searchText);
-        goToFavorites = (Button)findViewById(R.id.gotoFavorites);
-        fontSizeMinus = (Button)findViewById(R.id.fontsizeminus);
-        fontSizePlus = (Button)findViewById(R.id.fontsizeplus);
-        fontExample = (TextView)findViewById(R.id.fontexample);
-    }
-
-    private void setButtonsListener() {
-        searchButton.setOnClickListener(this);
-        goToFavorites.setOnClickListener(this);
-        fontSizeMinus.setOnClickListener(this);
-        fontSizePlus.setOnClickListener(this);
-    }
-
-    private void startNewActivity(Class another){
-        Intent intent = new Intent(MainActivity.this, another);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
+    @OnClick({
+            R.id.searchButton,
+            R.id.gotoFavorites,
+            R.id.fontsizeminus,
+            R.id.fontsizeplus
+    })
+    void onButtonClick(Button button){
+        switch (button.getId()){
             case R.id.searchButton:
-                if(searchText.getText().toString().length()>0){
-                    startNewActivity(SearchResultsList.class);
-                }
+                if(searchText.getText().toString().length()>0)
+                    startNewActivity(SearchResultsList.class, true);
                 break;
 
             case R.id.gotoFavorites:
-                startNewActivity(FavoritePoems.class);
+                startNewActivity(FavoritePoems.class, false);
                 break;
 
             case R.id.fontsizeminus:
@@ -118,5 +97,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 }
                 break;
         }
+    }
+
+    private void startNewActivity(Class another, boolean hasExtra){
+        Intent intent = new Intent(MainActivity.this, another);
+        if(hasExtra) intent.putExtra("searchText",searchText.getText().toString());
+        startActivity(intent);
     }
 }

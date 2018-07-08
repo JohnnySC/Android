911058f31f6a7bd59f4d40963bbbf83b6c720511
Асатрян Оганнес <OSAsatryan.SBT@sberbank.ru>
2018-07-08 22:06:24 +0300

    removed old code

--- TestAppIntechRetrofit2/app/src/main/java/github/johnnysc/testappintechretrofit2/main/bean/SongList.kt
+++
@@ -1,8 +0,0 @@
-package github.johnnysc.testappintechretrofit2.main.di.bean
-
-import com.google.gson.annotations.SerializedName
-
-/**
- * @author Asatryan on 08.07.18
- */
-data class SongList(@SerializedName("results") val songsList : List<Song>)
